221f90e79299b95908a6b691175311356e850b37
Gregan <343314@gmail.com>
2022-10-12 14:58:49 +0300

    Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,10 @@
+﻿ // Задача 23
+
+ Console.Clear();
+ Console.Write("Введите число: ");
+
+	 int n = Convert.ToInt32(Console.ReadLine());
+
+ 	for (int i = 1; i <= n; i++)
+
+     Console.Write($"{i * i * i} ");
